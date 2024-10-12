e3c574119ac702e2f66eec5236344e8205807dce
LinuxMistery00 <eduardosbarros.ed@gmail.com>
2024-10-12 15:23:25 -0300

    User

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest'),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
